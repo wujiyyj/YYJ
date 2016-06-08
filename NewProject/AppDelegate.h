68adbed1fd1ef9e00142d8a6829d9e30f59e7c5f
yyj <787127430@qq.com>
2016-06-08 17:07:39 +0800

    test

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Xmj on 16/5/31.
+//  Copyright © 2016年 Xmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject
+//
+//  Created by Xmj on 16/5/31.
+//  Copyright © 2016年 Xmj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
